16a4040d91e29b1b08c9e0f0ad6eb7adb5b4c33e
liatmoss <liatmoss@hotmail.com>
2019-12-18 11:32:59 +0000

    refactored account to test behaviour of balance rather than state

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,6 @@
 class Account
   DEFAULT_BALANCE = 0
-  attr_reader :balance, :statement
+  attr_reader :statement
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
@@ -9,11 +9,13 @@ class Account
 
   def deposit(amount)
     @statement << "#{Time.now.strftime('%d/%m/%Y')} || #{amount}.00 || || #{@balance += amount}.00"
+    @balance
   end
 
   def withdraw(amount)
     raise "Not enough funds. Current balance is #{@balance}.00" if amount > @balance
 
     @statement << "#{Time.now.strftime('%d/%m/%Y')} || || #{amount}.00 || #{@balance -= amount}.00"
+    @balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,16 +5,14 @@ describe Account do
 
   describe '#deposit' do
     it 'expects the balance to equal 500 after a deposit of 500' do
-      amount.deposit(500.00)
-      expect(amount.balance).to eq(500.00)
+      expect(amount.deposit(500.00)).to eq(500)
     end
   end
 
   describe '#credit' do
     it 'expects the balance to equal 2500 after a withdrawal of 500' do
       amount.deposit(3000.00)
-      amount.withdraw(500.00)
-      expect(amount.balance).to eq(2500.00)
+      expect(amount.withdraw(500.00)).to eq(2500.00)
     end
 
     it 'raises an error if balance is less than withdrawal amount' do
